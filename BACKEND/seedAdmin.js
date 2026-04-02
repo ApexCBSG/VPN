b6f2faafcb59b9f2af123d68e8761f02687254e6
AbdulAHAD968 <ahad06074@gmail.com>
2026-04-02 09:47:48 +0500

    updated admin pass for injection

--- BACKEND/seedAdmin.js
+++ BACKEND/seedAdmin.js
@@ -11,16 +11,16 @@ const seedAdmin = async () => {
     console.log('MongoDB Connected for seeding...');
 
     const email = 'admin@gmail.com';
-    const password = '12345678';
+    const password = 'admin123';
 
-    
+
     let user = await User.findOne({ email });
 
     if (user) {
       console.log('Admin user already exists. Updating privileges...');
       user.role = 'admin';
       user.isVerified = true;
-      user.password = password; 
+      user.password = password;
       await user.save();
     } else {
       console.log('Creating new administrative account...');
@@ -37,7 +37,7 @@ const seedAdmin = async () => {
     console.log('Administrative account initialized successfully.');
     console.log('Email:', email);
     console.log('Role: Administrator');
-    
+
     process.exit(0);
   } catch (err) {
     console.error('Seeding selection failure:', err);
